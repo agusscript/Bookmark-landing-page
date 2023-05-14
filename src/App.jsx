00d1fc23069030e04a53f952534408a065f0e4c5
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-05-14 13:18:52 -0300

    Add initial files, install eslint

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+function App() {
+  return (
+    <>
+      <h1>
+        bookmark react landing page
+      </h1>
+    </>
+  );
+}
+
+export default App;
